63019adfc0cffe0be357e676556f2c5d6d92ccd7
Federico Marchi <federicomarchi98@gmail.com>
2022-10-22 19:04:04 +0200

    Update.

--- indice_di_finalizzazione.py
+++ indice_di_finalizzazione.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-# import altair as alt
+import altair as alt
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
@@ -64,27 +64,36 @@ def main():
     data["i_f"] = data.npg / data.team_npxG
     data = data.sort_values(by=["i_f"], ascending=False)
 
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 4))
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.spines["bottom"].set_visible(False)
-    ax.spines["left"].set_visible(False)
+    # fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 4))
+    # ax.spines["top"].set_visible(False)
+    # ax.spines["right"].set_visible(False)
+    # ax.spines["bottom"].set_visible(False)
+    # ax.spines["left"].set_visible(False)
 
-    sns.barplot(data=data, y="team", x="i_f", orient="h", ax=ax, color="#11ad8e")
-    ax.set_xticks([], [])
-    ax.set(xlabel="", ylabel="")
-    ax.bar_label(
-        ax.containers[-1],
-        fmt="%.2f",
-        label_type="center",
-        color="#ffffff",
+    # sns.barplot(data=data, y="team", x="i_f", orient="h", ax=ax, color="#11ad8e")
+    # ax.set_xticks([], [])
+    # ax.set(xlabel="", ylabel="")
+    # ax.bar_label(
+    #     ax.containers[-1],
+    #     fmt="%.2f",
+    #     label_type="center",
+    #     color="#ffffff",
+    # )
+    # for label in ax.yaxis.get_ticklabels():
+    #     label.set_color("#0cfaca")
+
+    # st.plotly_chart(fig, dpi=800, use_container_width=True)
+
+    bar_chart = (
+        alt.Chart(data)
+        .mark_bar()
+        .encode(
+            y="team:O",
+            x="i_f:Q",
+            # color="EnergyType:N"
+        )
     )
-    for label in ax.yaxis.get_ticklabels():
-        label.set_color("#0cfaca")
-
-    st.pyplot(fig, dpi=800)
-    # plot = px.bar(data_frame=data, x="i_f", y="team", orientation="h")
-    # st.plotly_chart(plot)
+    st.altair_chart(bar_chart, use_container_width=True)
 
     st.header("Com'è calcolato l'indice di finalizzazione?")
     equation = Image.open(GRAPHIC_PATH / "formula.png")
@@ -93,30 +102,11 @@ def main():
     st.write("npG: gol segnati senza rigori, npxG: gol attesi senza rigori.")
     st.markdown("##")
 
-    cols = st.columns((0.8, 0.2))
-    cols[0].subheader("Spiegazione dell'indice:")
-    cols[0].write(
-        "Post: [Instagram](https://www.instagram.com/p/CjA3uLFt85o/?utm_source=ig_web_copy_link) Articolo: [Blog](http://www.sigmaeffe.it/2022/09/fantacalcio-quali-squadre-di-serie.html)"
+    st.write(
+        "Post di spiegazione: [Instagram](https://www.instagram.com/p/CjA3uLFt85o/?utm_source=ig_web_copy_link)",
+        "Articolo: [Blog](http://www.sigmaeffe.it/2022/09/fantacalcio-quali-squadre-di-serie.html)",
+        "Dati: [understat](https://understat.com/)",
     )
-    cols[1].subheader("Fonte dati")
-    cols[1].write("Understat: [understat.com](https://understat.com/)")
-
-    # st.markdown("##")
-    # st.markdown("##")
-    # data.index = data.team
-    # data = data[["npg", "team_npxG", "i_f"]]
-    # data.rename(columns={"team_npxG": "npxG"}, inplace=True)
-    # data.npg = data.npg.astype("int")
-    # st.header("Tutti i dati")
-    # st.dataframe(
-    #     data=data.style.format(precision=3).apply(
-    #         lambda x: [
-    #             "background-color: " + PRIMARY_COLOR if x.name == "i_f" else ""
-    #             for _ in data.index
-    #         ],
-    #         axis=0,
-    #     )
-    # )
 
     st.markdown("##")
 
@@ -124,14 +114,12 @@ def main():
     bottom_image = Image.open(GRAPHIC_PATH / "nome_logo_slogan_streamlit.png")
 
     st.markdown("##")
-    st.header("Creato da [SIGMAEFFE](http://www.sigmaeffe.it)")
     cols = st.columns((0.2, 0.6, 0.2))
     cols[1].image(bottom_image)
 
     st.markdown("##")
     st.markdown("##")
     cols = st.columns((0.3, 0.05, 0.1, 0.05, 0.3))
-    cols[0].subheader("Seguimi su")
     cols[1].markdown(
         '<a href="https://www.instagram.com/sigmaeffe"><img alt="Instagram" src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" width=40>',
         unsafe_allow_html=True,
